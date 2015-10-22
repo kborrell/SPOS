43a2603b89297a64396635f428348c406f6ed404
Kevin Borrell <kborrells@gmail.com>
2015-10-22 21:41:34 +0200

    First commit.

---
+++ src/main/java/com/udl/tfg/sposapp/SposAppApplication.java
@@ -0,0 +1,12 @@
+package com.udl.tfg.sposapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SposAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SposAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/udl/tfg/sposapp/SposAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.udl.tfg.sposapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SposAppApplication.class)
+@WebAppConfiguration
+public class SposAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
