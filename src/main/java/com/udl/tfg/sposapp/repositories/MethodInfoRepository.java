3649286cf6bd18cb1840c27ca39500f4ff56dd8e
Kevin Borrell <kborrells@gmail.com>
2015-12-03 01:50:04 +0100

    Added search methods to model and methods repositories.

--- src/main/java/com/udl/tfg/sposapp/repositories/MethodInfoRepository.java
+++ src/main/java/com/udl/tfg/sposapp/repositories/MethodInfoRepository.java
@@ -1,9 +1,15 @@
 package com.udl.tfg.sposapp.repositories;
 
+import com.udl.tfg.sposapp.models.MethodCodes;
 import com.udl.tfg.sposapp.models.MethodInfo;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "methods", path = "methods")
 public interface MethodInfoRepository extends PagingAndSortingRepository<MethodInfo, Long> {
+
+    List<MethodCodes> findByMethod(@Param("modelName") MethodCodes model);
 }
--- src/main/java/com/udl/tfg/sposapp/repositories/ModelInfoRepository.java
+++ src/main/java/com/udl/tfg/sposapp/repositories/ModelInfoRepository.java
@@ -1,10 +1,16 @@
 package com.udl.tfg.sposapp.repositories;
 
+import com.udl.tfg.sposapp.models.MethodCodes;
+import com.udl.tfg.sposapp.models.ModelCodes;
 import com.udl.tfg.sposapp.models.ModelInfo;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "models", path = "models")
 public interface ModelInfoRepository extends PagingAndSortingRepository<ModelInfo, Long> {
 
+    List<ModelInfo> findByModel(@Param("modelName") ModelCodes model);
 }
