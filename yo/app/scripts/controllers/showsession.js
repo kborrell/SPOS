1202f81ac410b6f35c53f839c10ada0134349916
Kevin Borrell <kborrells@gmail.com>
2016-01-21 00:10:25 +0100

    Implemented login and session query.

--- yo/app/scripts/controllers/showsession.js
+++ yo/app/scripts/controllers/showsession.js
@@ -11,15 +11,25 @@ angular.module('sposApp')
   .controller('ShowSessionCtrl', function ($scope, $http, $stateParams, Session) {
       $scope.sessionKey = $stateParams.key;
       $scope.sessionId = $stateParams.id;
-
+      $scope.session = null;
       $scope.logged = false;
 
       $scope.init = function () {
         $scope.logged = $scope.sessionKey && $scope.sessionId;
+        if ($scope.logged)
+          GetSession();
       };
 
       $scope.logInSession = function(){
         $scope.logged = true;
+        GetSession();
+      };
+
+      var GetSession = function () {
+        Session.query({id: $scope.sessionId, key: $scope.sessionKey})
+          .$promise.then(function (session) {
+            $scope.session = session;
+        });
       };
 
       $scope.init();
