2e289c83c3134d82369b838abdfa25f4be73b822
Kevin Borrell <kborrells@gmail.com>
2016-01-23 17:44:29 +0100

    Implemented session information summary in view session section.

--- yo/app/scripts/controllers/showsession.js
+++ yo/app/scripts/controllers/showsession.js
@@ -8,11 +8,12 @@
  * Controller of the sposApp
  */
 angular.module('sposApp')
-  .controller('ShowSessionCtrl', function ($scope, $http, $stateParams, Session) {
+  .controller('ShowSessionCtrl', function ($scope, $sce, $http, $stateParams, Session) {
       $scope.sessionKey = $stateParams.key;
       $scope.sessionId = $stateParams.id;
       $scope.session = null;
       $scope.logged = false;
+      $scope.sessionStatus = "";
 
       $scope.init = function () {
         $scope.logged = $scope.sessionKey && $scope.sessionId;
@@ -29,7 +30,19 @@ angular.module('sposApp')
         Session.query({id: $scope.sessionId, key: $scope.sessionKey})
           .$promise.then(function (session) {
             $scope.session = session;
+            GetSessionStatus();
         });
+      };
+
+      var GetSessionStatus = function () {
+        if ($scope.session.vmConfig.IP == null && $scope.session.results == null)
+          $scope.sessionStatus = $sce.trustAsHtml("<span style=\"color: #FF5722;\"> Not started </span>");
+
+        if ($scope.session.vmConfig.IP != null && $scope.session.results == null)
+          $scope.sessionStatus = $sce.trustAsHtml("<span style=\"color: #FFC107;\"> Executing </span>");
+
+        if ($scope.session.vmConfig.IP == null && $scope.session.results != null)
+          $scope.sessionStatus = $sce.trustAsHtml("<span style=\"color: #4CAF50;\"> Finished </span>");
       };
 
       $scope.init();
